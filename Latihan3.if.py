e5f66d26db4c2e1d2bcc1610082ea9f536d7420a
Rio Saputra <riobadrun1721@gmail.com>
2021-11-18 19:12:12 +0700

    program perulangan bertingkat

---
+++ Latihan3.if.py
@@ -0,0 +1,8 @@
+# perulangan bertingkat
+for i in range(0, 10):
+    # nested loop
+    # to iterate from 1 to 10
+    for j in range(0,10):
+        # print multiplication
+        print(i + j, end=' ')
+    print() 
