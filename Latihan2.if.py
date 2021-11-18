c13a1567bf0e47fd27347e7d19c6ce3ba3207a0e
Rio Saputra <riobadrun1721@gmail.com>
2021-11-18 19:06:24 +0700

    program mengurutkan data

---
+++ Latihan2.if.py
@@ -0,0 +1,16 @@
+#program mengurutkan data dari yang terkecil
+print(20*"=")
+a = input("masukan bil 1 = ")
+b = input("masukan bil 2 = ")
+c = input("masukan bil 3 = ")
+d = input("masukan bil 4 = ")
+e = input("masukan bil 5 = ")
+f = input("masukan bil 6 = ")
+g = input("masukan bil 7 = ")
+
+data = [a, b, c, d, e, f, g]
+data.sort()
+
+print("ini adalah urutan angka yang terkecil dari bilangan 1...7 = ", data)
+
+print(20*"=")
