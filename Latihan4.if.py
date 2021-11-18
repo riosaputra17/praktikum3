618274930ce36889721040b20517aec54d868781
Rio Saputra <riobadrun1721@gmail.com>
2021-11-18 19:17:41 +0700

    program bilangan acak yang lebih kecil dari 0.5

---
+++ Latihan4.if.py
@@ -0,0 +1,11 @@
+print(20*"=")
+
+print("masukan nilai n: 5")
+import random
+jumlah = 5
+a = 0
+for x in range (jumlah):
+    i = random.uniform (.0,.5)
+    a+=1
+    print("data ke = ",a,"===>",i)
+print("selesai")
