b1761cfcbb3fc301e95f72e87c9a67ccc6f3351a
Rio Saputra <riobadrun1721@gmail.com>
2021-11-18 18:53:16 +0700

    program input 2 buah bilangan Terbesar dan terkecil

---
+++ Latihan1.if.py
@@ -0,0 +1,15 @@
+# peogram menentukan bilangan terbesar
+
+print(20*"=")
+
+a =int(input("masukan bilangan ke 1 = "))
+b =int(input("masukan bilangan ke 2 = "))
+
+if a > b :
+    print("bilangan ke 1 yang terbesar")
+elif b > a :
+    print("bilangan ke 2 yang terbesar")
+else:
+    print("kesalahan")
+
+print(20*"=") 
